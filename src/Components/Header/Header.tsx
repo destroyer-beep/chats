844641285beebb5ed6669f3510cdfa50a1d33876
Vladimir <destroyer.hramov@yandex.ru>
2023-06-21 00:27:20 +0300

    added userlice

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-const Header = () => {
+const Header:React.FC = () => {
+
     return <div>
 
     </div>
---
+++ src/store/userSlice.ts
@@ -0,0 +1,30 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+
+type User = {
+    id:  number | null;
+    name: string | null;
+}
+
+const initialState:User = {
+    id: null,
+    name: null
+}
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        addUser(state, action:PayloadAction<User>) {
+            state.id = action.payload.id;
+            state.name = action.payload.name;
+        },
+        removeUser(state, action) {
+            state.id = null;
+            state.name = null;
+        },
+    }
+})
+
+export default userSlice.reducer;
+export const {addUser, removeUser} = userSlice.actions;
